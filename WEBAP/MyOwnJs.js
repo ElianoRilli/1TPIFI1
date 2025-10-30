bcd3daa3b95a957686d80bbb44a356c0a75b50da
Your Name <you@example.com>
2025-10-30 14:40:27 +0100

    aa

--- WEBAP/MyOwnJs.js
+++ WEBAP/MyOwnJs.js
@@ -1,2 +1,4 @@
-
-document.getElementById("ChangeThis").innerHTML ="Hello World"
+$(function (){
+    $("#ChangeThis").text("Hello World :D");
+});
+/* document.getElementById("ChangeThis").innerHTML ="Hello World" */
