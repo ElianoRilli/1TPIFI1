39d4a6167dbe00cbe2b3be1bda45bfd5ac49680a
Your Name <you@example.com>
2026-01-14 13:16:19 +0100

    fruits

--- WEBAP/Last year exam/provided files/frontEnd.js
+++ WEBAP/Last year exam/provided files/frontEnd.js
@@ -1,34 +1,60 @@
-$(start);
+$(document).ready(function () {
+    function loadFruits(selectedId) {
+        $.getJSON("getFruits.php", function (fruits) {
 
-function start() {
-    $.getJSON("getFruits.php", function (fruits) {
-        var select = $("#Fruits");
-        select.empty();
-        for (var i = 0; i < fruits.length; i++) select.append('<option value="' + fruits[i].fruitId + '">' + fruits[i].fruitName + '</option>');
+            var select = $("#Fruits");
+            select.empty();
 
-        select.change(function () {
-            var fruitId = $(this).val();
+            for (var i = 0; i < fruits.length; i++) {
+                select.append('<option value="' + fruits[i].fruitId + '">' + fruits[i].fruitName + '</option>');
+            }
+
+            if (selectedId) {
+                select.val(selectedId);
+            } else {
+                selectedId = select.val();
+            }
+
+            for (var i = 0; i < fruits.length; i++) {
+                if (fruits[i].fruitId == selectedId) {
+                    showFruit(fruits[i]);
+                    break;
+                }
+            }
+        });
+    }
+
+    function showFruit(fruit) {
+        if (fruit.availability == 0) {
+            $("#FruitData").text("Out of stock");
+            $("#FruitOrder").html("");
+        } else {
+            $("#FruitData").text(fruit.availability + " in stock");
+            $("#FruitOrder").html('<input type="number" placeholder="quantity"> <button id="btnOrder">Order</button>');
+        }
+    }
+
+    $("#Fruits").change(function () {
+        var fruitId = $(this).val();
+        $.getJSON("getFruits.php", function (fruits) {
             for (var i = 0; i < fruits.length; i++) {
                 if (fruits[i].fruitId == fruitId) {
-                    if (fruits[i].availability == 0) {
-                        $("#FruitData").text("Out of stock");
-                        $("#FruitOrder").html("");
-                    } else {
-                        $("#FruitData").text(fruits[i].availability + " in stock");
-                        $("#FruitOrder").html("<input type='number' placeholder='quantity'> <button id='btnOrder'>Order</button>");
-
-                    
-                        $("#btnOrder").click(function () {
-                            var quantity = $("#FruitOrder input").val();
-                            if (quantity < 0) {
-                                alert("Please enter a valid order");
-                            }
-                        });
-                    }
+                    showFruit(fruits[i]);
                     break;
                 }
             }
         });
     });
-}
 
+    $(document).on("click", "#btnOrder", function () {
+        var quantity = $("#FruitOrder input").val();
+        var fruitId = $("#Fruits").val();
+
+        $.post("getFruits.php", { fruitId: fruitId, quantity: quantity }, function (result) {
+            $("#OrderResult").text(result);
+            loadFruits(fruitId); 
+        });
+    });
+
+    loadFruits();
+});
--- WEBAP/Last year exam/provided files/getFruits.php
+++ WEBAP/Last year exam/provided files/getFruits.php
@@ -1,8 +1,51 @@
 <?php
 $mysqli = new mysqli("localhost","root","","fruits");
-$result = $mysqli->query("SELECT fruitId, fruitName, availability FROM fruits");
-$fruits = [];
-while($row = $result->fetch_assoc()) $fruits[] = $row;
-echo json_encode($fruits);
+
+if ($mysqli->connect_errno) {
+    echo "Failed to connect to database";
+    exit();
+}
+
+// If no POST, return fruits as JSON
+if (!isset($_POST['fruitId'])) {
+    $result = $mysqli->query("SELECT fruitId, fruitName, availability FROM fruits");
+    $fruits = [];
+    while($row = $result->fetch_assoc()) $fruits[] = $row;
+    echo json_encode($fruits);
+    $mysqli->close();
+    exit();
+}
+
+// Get POST values
+$fruitId = intval($_POST['fruitId']);
+$quantity = intval($_POST['quantity']);
+
+// Check fruit exists
+$result = $mysqli->query("SELECT availability FROM fruits WHERE fruitId=$fruitId");
+if ($result->num_rows == 0) {
+    echo "Unknown given fruit";
+    $mysqli->close();
+    exit();
+}
+
+$row = $result->fetch_assoc();
+$available = intval($row['availability']);
+
+// Check quantity
+if ($quantity < 0) {
+    echo "Please enter a valid order";
+} else if ($quantity > $available) {
+    echo "We are unable to honor that request";
+} else if ($quantity == $available) {
+    // Case d: empty stock
+    $mysqli->query("UPDATE fruits SET availability=0 WHERE fruitId=$fruitId");
+    echo "You have emptied our stock";
+} else {
+    // Case e: reduce stock
+    $newStock = $available - $quantity;
+    $mysqli->query("UPDATE fruits SET availability=$newStock WHERE fruitId=$fruitId");
+    echo "Order placed successfully";
+}
+
 $mysqli->close();
 ?>
